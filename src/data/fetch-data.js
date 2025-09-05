ee9604f2745baad77c52c3b1ce7a6913ccd9e9c0
Hugo Perez <hugopereztm12309@gmail.com>
2025-09-05 19:15:10 -0400

    Add error handling with try catch

--- src/data/fetch-data.js
+++ src/data/fetch-data.js
@@ -1,8 +1,12 @@
 export default async function fetchData(location) {
-  const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(location)}?key=96QU7AHGNAX33HEE9XKBN7279`,
-    { mode: "cors" },
-  );
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(location)}?key=96QU7AHGNAX33HEE9XKBN7279`,
+      { mode: "cors" },
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
