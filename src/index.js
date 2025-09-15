8df9df21cd179dc7e15ba5f4a00a5f45dd9c2307
Hugo Perez <hugopereztm12309@gmail.com>
2025-09-15 18:48:19 -0400

    Initialize any drop down menus functionality

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import "./styles.css";
 import fetchData from "./data/fetch-data";
 import processJson from "./data/process-json";
 import displayData from "./data/display-data";
+import dropdown from "./utils/dropdown";
+
+dropdown();
 
 const form = document.getElementById("search-city-form");
 const searchInput = document.getElementById("search-bar");
