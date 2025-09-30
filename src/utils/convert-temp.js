23a26f18d64fa75e62e04b8c56430828062cf279
Hugo Perez <hugopereztm12309@gmail.com>
2025-09-30 19:03:23 -0400

    Formatting

--- src/utils/convert-temp.js
+++ src/utils/convert-temp.js
@@ -2,5 +2,5 @@ export function fahrenheitToCelsius(fahrenheit) {
   return ((fahrenheit - 32) * 5) / 9;
 }
 export function celsiusToFahrenheit(celsius) {
-    return (celsius * 9 / 5) + 32;
+  return (celsius * 9) / 5 + 32;
 }
