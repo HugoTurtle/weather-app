83c9ac3128ab39a596685383e3eee8d0a38d1d98
Hugo Perez <hugopereztm12309@gmail.com>
2025-09-30 17:27:23 -0400

    Store all ID elements and its values

--- src/data/display-data.js
+++ src/data/display-data.js
@@ -1,3 +1,8 @@
 export default async function displayData(weatherObjPromise) {
   const weatherObj = await weatherObjPromise;
+  const allElements = document.querySelectorAll("[id]");
+
+  allElements.forEach((element) => {
+    const key = element.id;
+  });
 }
