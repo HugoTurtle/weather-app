1c55532d0195a054683c335016fee03a6486cb6e
Hugo Perez <hugopereztm12309@gmail.com>
2025-10-05 18:53:50 -0400

    Add fahrenheit to celsius temperature conversion

--- src/data/modify-data.js
+++ src/data/modify-data.js
@@ -1,12 +1,20 @@
+import { fahrenheitToCelsius } from "../utils/convert-temp";
+
 export function modifyTempData(weatherPromise, tempScale) {
   return weatherPromise.then((weatherData) => {
-    switch(tempScale) {
-        case "fahrenheit":
-            break;
-        case "celsius":
-            break;
-        default:
-            console.log("Error with temperature scale inputted");
+    switch (tempScale) {
+      case "fahrenheit":
+        weatherData.temp = fahrenheitToCelsius(weatherData.temp);
+        weatherData.tempMax = fahrenheitToCelsius(weatherData.tempMax);
+        weatherData.tempMin = fahrenheitToCelsius(weatherData.tempMin);
+        weatherData.feelsLikeTemp = fahrenheitToCelsius(
+          weatherData.feelsLikeTemp,
+        );
+        break;
+      case "celsius":
+        break;
+      default:
+        console.log("Error with temperature scale inputted");
     }
     return weatherData;
   });
