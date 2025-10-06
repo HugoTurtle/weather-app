9376f4071fe4eef77a59f5649c8c0133447c9ad3
Hugo Perez <hugopereztm12309@gmail.com>
2025-10-06 18:37:05 -0400

    Formatting

--- src/utils/toggle-temperature-cycle.js
+++ src/utils/toggle-temperature-cycle.js
@@ -2,9 +2,9 @@ export function toggleTemp(temperatureScale) {
   return temperatureScale === "fahrenheit" ? "celsius" : "fahrenheit";
 }
 export function changeTempDisplay(btn, temperatureScale) {
-    if(temperatureScale === "fahrenheit") {
-        btn.textContent = "Fahrenheit"
-    } else if (temperatureScale === "celsius") {
-        btn.textContent = "Celsius"
-    }
+  if (temperatureScale === "fahrenheit") {
+    btn.textContent = "Fahrenheit";
+  } else if (temperatureScale === "celsius") {
+    btn.textContent = "Celsius";
+  }
 }
