2537982b34409d55e49e133e9f98817618dbf1a8
Hugo Perez <hugopereztm12309@gmail.com>
2025-10-06 19:05:14 -0400

    Add functionality to temperature button
    Allows the user to toggle between celsius and fahrenheit. As well as
    begin with the default of fahrenheit when a from is submitted

--- src/utils/weather-controller.js
+++ src/utils/weather-controller.js
@@ -1,21 +1,42 @@
-import processJson from "../data/process-json";
 import fetchData from "../data/fetch-data";
-import displayData from "../data/display-data"
+import processJson from "../data/process-json";
+import displayData from "../data/display-data";
+import { modifyTempData } from "../data/modify-data";
+import { toggleTemp, changeTempDisplay } from "./toggle-temperature-cycle";
 
 export default function weatherController() {
-  let weatherPromise;
+  let temperatureScale;
+  let toggleTempButton = false;
   let searchTerm;
+  let weatherPromise;
 
   const form = document.getElementById("search-city-form");
   const searchInput = document.getElementById("search-bar");
+  const tempButton = document.getElementById("tempBtn");
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
+    defaultTemperatureCycle(tempButton);
 
+    toggleTempButton = true;
     searchTerm = searchInput.value.trim();
     weatherPromise = getProcessedData(searchTerm);
     displayData(weatherPromise);
   });
+
+  tempButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (toggleTempButton === true) {
+      displayData(modifyTempData(weatherPromise, temperatureScale));
+      changeTempDisplay(tempButton, temperatureScale);
+      temperatureScale = toggleTemp(temperatureScale);
+    }
+  });
+  function defaultTemperatureCycle(btn) {
+    temperatureScale = "fahrenheit";
+    changeTempDisplay(btn, "celsius");
+  }
 }
 function getProcessedData(location) {
   if (location) return processJson(fetchData(location), 0);
