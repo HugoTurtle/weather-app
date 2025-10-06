9f08f7f2ae5105844c61a00ad08783b017aabb52
Hugo Perez <hugopereztm12309@gmail.com>
2025-10-06 17:08:04 -0400

    Formatting

--- src/utils/convert-temp.js
+++ src/utils/convert-temp.js
@@ -1,5 +1,5 @@
 function roundToHundredsValue(num) {
-  return Math.round((num + Number.EPSILON) * 100) / 100
+  return Math.round((num + Number.EPSILON) * 100) / 100;
 }
 export function fahrenheitToCelsius(fahrenheit) {
   return roundToHundredsValue(((fahrenheit - 32) * 5) / 9);
