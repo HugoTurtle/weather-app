c71ec75907569ccdbe847df861727378cf16fe09
Hugo Perez <hugopereztm12309@gmail.com>
2025-10-06 18:35:37 -0400

    Add functions to update temperature scale

---
+++ src/utils/toggle-temperature-cycle.js
@@ -0,0 +1,10 @@
+export function toggleTemp(temperatureScale) {
+  return temperatureScale === "fahrenheit" ? "celsius" : "fahrenheit";
+}
+export function changeTempDisplay(btn, temperatureScale) {
+    if(temperatureScale === "fahrenheit") {
+        btn.textContent = "Fahrenheit"
+    } else if (temperatureScale === "celsius") {
+        btn.textContent = "Celsius"
+    }
+}
